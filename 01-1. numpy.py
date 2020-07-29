949a4152d05953a524dd696692717bc8a93425f4
FatherA <seojun.a.kim@gmail.com>
2020-07-29 13:41:02 +0900

    am

---
+++ 01-1. numpy.py
@@ -0,0 +1,14 @@
+# import numpy as np
+# a=[1,2,3,4]
+# a=np.array(a).reshape((2,2))
+# print(a)
+#
+# print(a+5)
+
+import numpy as np
+a = np.array([1,2,3])
+b = np.array([2,4,6])
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
