dfb9c085b2864b671d02515d7637c2973110aa7f
FatherA <seojun.a.kim@gmail.com>
2020-08-04 14:39:12 +0900

    pm1

--- 05-2. mnist prac.py
+++ 05-2. mnist prac.py
@@ -1,10 +1,37 @@
-import sys, os
-sys.path.append(os.pardir)
-from dataset.mnist import load_mnist
+# import sys, os
+# sys.path.append(os.pardir)
+# from dataset.mnist import load_mnist
+#
+# (x_train,t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+#
+# print(x_train.shape)
+# print(t_train.shape)
+# print(x_test.shape)
+# print(t_test.shape)
 
-(x_train,t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+# 2.
+# from PIL import Image
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mgimg
+#
+# img = Image.open('아이린.jpg')
+# img_pixel = np.array(img)
+# plt.imshow(img_pixel)
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+import pickle
+
+def init_network():
+    with open("D:/dl/sample_weight.pkl","rb") as f:
+        network = pickle.load(f)
+    return network
+
+network = init_network()
+
+print(network["W1"].shape)
+print(network["W2"].shape)
+print(network["W3"].shape)
+print(network["b1"].shape)
+print(network["b2"].shape)
+print(network["b3"].shape)
+
--- deep-learning-from-scratch-master/ch03/mnist_show.py
+++ deep-learning-from-scratch-master/ch03/mnist_show.py
@@ -10,14 +10,15 @@ def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False, one_hot_label=True)
 
 img = x_train[0]
-label = t_train[0]
-print(label)  # 5
-
-print(img.shape)  # (784,)
-img = img.reshape(28, 28)  # 형상을 원래 이미지의 크기로 변형
-print(img.shape)  # (28, 28)
-
-img_show(img)
+# label = t_train[0]
+# print(label)  # 5
+#
+# print(img.shape)  # (784,)
+# img = img.reshape(28, 28)  # 형상을 원래 이미지의 크기로 변형
+# print(img.shape)  # (28, 28)
+#
+# img_show(img)
+print(t_train[0])
